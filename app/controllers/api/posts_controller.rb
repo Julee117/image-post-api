7504f91c68b7211996de692384642059166ca610
github username <juliannne@gmail.com>
2018-02-20 15:21:57 -0500

    Add show, before_action and create methods

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,6 +1,21 @@
 class Api::PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
   def index
     render json: Post.all
+  end
+
+  def show
+    render json: @post
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: {message: post.errors}
+    end
   end
 
   private
