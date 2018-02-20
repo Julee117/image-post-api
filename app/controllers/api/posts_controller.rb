ee82ff87df2fbdf8534312cb6e92ef55d36ad0b0
github username <juliannne@gmail.com>
2018-02-20 15:55:02 -0500

    Add update, destroy methods

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,19 +2,36 @@ class Api::PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
 
   def index
-    render json: Post.all
+    @posts = Post.all
+    render json: @posts, status: 200
   end
 
   def show
-    render json: @post
+    render json: @post, status: 200
   end
 
   def create
     post = Post.new(post_params)
     if post.save
-      render json: post
+      render json: post, status: 201
     else
-      render json: {message: post.errors}
+      render json: {message: post.errors}, status: 422
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: {message: post.errors}, status: 422
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      render status: 204
+    else
+      render json: {message: "Unable to remove post"}, status: 400
     end
   end
 
