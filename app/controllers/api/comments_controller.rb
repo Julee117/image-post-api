ec713ab2c5df8b4509debe8423f4c602e767f9ec
github username <juliannne@gmail.com>
2018-02-24 17:13:59 -0500

    Add before_action, show and create methods

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,7 +1,22 @@
 class Api::CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
+
   def index
     @comments = Comment.all
     render json: @comments, status: 200
+  end
+
+  def show
+    render json: @comment, status: 200
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: comment, status: 201
+    else
+      render json: {message: comment.errors}, status: 422
+    end
   end
 
   private
