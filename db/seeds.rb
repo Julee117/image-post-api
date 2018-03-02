f079b2ccc87dbd055f6e819ff9ea937b48fe824d
github username <juliannne@gmail.com>
2018-03-02 02:32:19 -0500

    Add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create(title: "War face", image_url: "http://quotationsquotes.com/wp-content/uploads/2015/10/Top-30-Funny-cat-Picture-Quotes-ROFL.jpg", content: "This cat is ready", likes: 0)
+Post.create(title: "Laughing pug", image_url: "https://lh6.ggpht.com/vd9dnqXiZMFcP7pjhw6JxeT_vIThDpJg2jN5rVW_f6-nJFuJzXBeiUBE9fskWFS7EQ=w300", content: "This guy makes me smile", likes: 0)
+Post.create(title: "Face plant", image_url: "https://pbs.twimg.com/media/CySyUNFXAAANL5i.jpg", content: "His favorite sleeping position", likes: 0)
+Post.create(title: "Corgi loaf", image_url: "http://i.imgur.com/yLkFUI5.jpg", content: "Corgi down", likes: 0)
