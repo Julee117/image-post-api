55c8b48f589f7e3fb0fba2967064379254b2bd15
github username <juliannne@gmail.com>
2018-03-14 23:46:46 -0400

    Modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Post.create(title: "War face", image_url: "http://quotationsquotes.com/wp-content/uploads/2015/10/Top-30-Funny-cat-Picture-Quotes-ROFL.jpg", content: "This cat is ready", likes: 0)
-Post.create(title: "Laughing pug", image_url: "https://lh6.ggpht.com/vd9dnqXiZMFcP7pjhw6JxeT_vIThDpJg2jN5rVW_f6-nJFuJzXBeiUBE9fskWFS7EQ=w300", content: "This guy makes me smile", likes: 0)
-Post.create(title: "Face plant", image_url: "https://pbs.twimg.com/media/CySyUNFXAAANL5i.jpg", content: "His favorite sleeping position", likes: 0)
-Post.create(title: "Corgi loaf", image_url: "http://i.imgur.com/yLkFUI5.jpg", content: "Corgi down", likes: 0)
-Post.create(title: "Cutey", image_url: "http://i.imgur.com/yF6Imbd.png", content: "Dog before and after being called a good boy", likes: 0)
+Post.create(title: "Seal", image_url: "http://res.cloudinary.com/du8aeeumk/image/upload/v1519974937/yxqpfx7mkuo0w4ze9zau.jpg", content: "Found a loose seal in my bathroom", likes: 0)
+Post.create(title: "Laughing pug", image_url: "http://res.cloudinary.com/du8aeeumk/image/upload/v1521058936/laughing-pug.jpg", content: "This guy makes me smile", likes: 0)
+Post.create(title: "Face plant", image_url: "http://res.cloudinary.com/du8aeeumk/image/upload/v1521059305/cat-giving-up.jpg", content: "His favorite sleeping position", likes: 0)
+Post.create(title: "Corgi loaf", image_url: "http://res.cloudinary.com/du8aeeumk/image/upload/v1521058954/corgi-loaf.png", content: "Corgi down", likes: 0)
+Post.create(title: "Smiley", image_url: "http://res.cloudinary.com/du8aeeumk/image/upload/v1519975740/nqsurseafjreqlwu5sdl.jpg", content: "Smiles are contagious", likes: 0)
